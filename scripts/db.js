2ced83cfc93e9c27c9657018798992212d4495b5
Érick Lúcio <104475806+ericklucioh@users.noreply.github.com>
2023-11-30 22:40:13 -0300

    criando o  dos dados

---
+++ scripts/db.js
@@ -0,0 +1,57 @@
+const servicos = [
+    {
+        id: 0,
+        img:'',
+        nome:'',
+    },
+    {
+        id: 1,
+        img:'',
+        nome:'',
+    },
+    {
+        id: 2,
+        img:'',
+        nome:'',
+    },
+];
+const diferenciais = [
+    {
+        id: 0,
+        img:'',
+        titulo: '',
+        paragrafo: ''
+    },
+    {
+        id: 1,
+        img:'',
+        titulo: '',
+        paragrafo: ''
+    },
+    {
+        id: 2,
+        img:'',
+        titulo: '',
+        paragrafo: ''
+    },
+];
+const avaliacao = [
+    {
+        id: 0,
+        nome: '',
+        avaliacao:'',
+        img: ''
+    },
+    {
+        id: 1,
+        nome: '',
+        avaliacao:'',
+        img: ''
+    },
+    {
+        id: 2,
+        nome: '',
+        avaliacao:'',
+        img: ''
+    }
+]
