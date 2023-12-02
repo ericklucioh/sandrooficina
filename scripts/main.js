53ec2941dde6799fcc43cfd8ccb2d45d5700c0ee
Érick Lúcio <104475806+ericklucioh@users.noreply.github.com>
2023-12-01 21:42:46 -0300

    as

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+
+function arrow(id) {
+    Element = document.getElementById(`${id}`);
+    Element.classList.add("teste");
+}
+function arrow1(id) {
+    Element = document.getElementById(`${id}`);
+    Element.classList.remove("teste");
+}
